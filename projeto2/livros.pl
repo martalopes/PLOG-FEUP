eaa8d7d0f7e6a78a2c9e8429ab2366b92819dd54
Marta Lopes <ei12106@fe.up.pt>
2015-12-12 00:50:04 +0000

    olha já mostra coisas

--- projeto2/livros.pl
+++ projeto2/livros.pl
@@ -1,69 +1,108 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
-
-/*[[l1,a1,t1,2005,alt1,larg1],.....]
-Apresentar:
-[[l1,l2],[l3,l4],[l5,l6,l7]]
-Computar:
-[[25,25],[20,20]]
-
-
-validrow([L,A,T,D,Al,La],P,SumLarg):-
-	La+SumLarg #<=LarguraPrat.
+%id, nome, autor, tema, 
+book(1, 'Memorial do Covento', 		'Saramago', 'Romance', 	1995, 3, 15).
+book(2, 'Lusiadas', 				'Camoes', 	'Classic', 	1995, 3, 15).
+book(3, 'Maias', 					'Eca', 		'Classic', 	1995, 3, 15).
+book(4, 'A Viagem do Elefante', 	'Saramago', 'Romance', 	1995, 2, 15).
 
 
+listBooks(List, MaxHeight) :-
+	findall([Id, Author,BookW, BookH],
+			book(Id, _, Author, _, _, BookW, BookH),
+			ListTemp),
+	checkBookHeight(ListTemp, ListTemp2, MaxHeight),
+	addShelfSpace(ListTemp2, List).
 
-validBookshelf([],_,_,_).
-validBookshelf(_,[],_,_).
-
-validBookshelf([Livro|Tail], [P|T1],[SumLarg|T2]}):-
-validrow(Livro,P,SumLarg);
-validBookshelf(Livro,T1,T2).
-*/
-
-
-vl([],_).
-vl([Livro|T],R):-
-	verificalargura(T,R1),
-	element(6, Livro, LarguraLivro),
-	sum(LarguraLivro,R1,R).
-
-verificalargura([Livro|T],Lmax):-
-	vl([Livro|T],R),
-	R #=< Lmax.
+checkBookHeight([],[],_).
+checkBookHeight([Book|T], BookResult, MaxHeight):-
+	nth0(3, Book, BookHeight),
+	BookHeight > MaxHeight,
+	write('Um livro não foi adicionado'), %EDITAR PARA FICAR BONITO COM NOME E ASSIM NÉ
+	checkBookHeight(T,  BookResult, MaxHeight).
 
 
-verificaaltura([],_).
-verificaaltura([Livro|T], Amax):-
-	verificaaltura(T, Amax),
-	element(5, Livro, AlturaLivro),
-	AlturaLivro #=< Amax.
+checkBookHeight([Book|T], [BResH|BResT],MaxHeight):-
+	nth0(3, Book, BookHeight),
+	BResH = Book,
+	checkBookHeight(T, BResT, MaxHeight).
 
 
-getprateleira([[]|Resto],[],Resto).
-getprateleira([Livro|T],[Livro|T2],Resto):-
-	getprateleira(T,T2,Resto).
-
-verificaprateleira([],_,_).
-verificaprateleira(L, Lmax, Amax):-
-	getprateleira(L,P,T),
-	verificaprateleira(T,Lmax,Amax).
-
-verificaLivro([],[]).
-verificaLivro([[A,B,C,D,E,F],[],[A1,B1,C1,D1,E1,F1]|T],[[A,B,C,D,E,F],[A1,B1,C1,D1,E1,F1]|T2]):-
-	verificaLivro([[A1,B1,C1,D1,E1,F1]|T],T2).
-verificaLivro([[A,B,C,D,E,F]|T],[[A,B,C,D,E,F]|T2]):-
-	verificaLivro(T,T2).
-
-verificaLivro(_,[]).
 
 
-livros(Livros, NrPrateleiras, LarguraPrat, AlturaPrat, Prateleiras):-
-LarguraPrat#>0,
-AlturaPrat#>0,
-NrPrateleiras#>0,permutation(Livros,NewLivros),
-verificaLivro(Prateleiras,NewLivros),
+getSizeOfBook(SizeOfBook, [_,_,SizeOfBook,_]).
 
-append(Prateleiras,L).
+setBookSpaces(Spaces, SizeOfBook):-
+	length(Spaces, SizeOfBook),
+	startSetEquals(Spaces).
+
+setEquals(_, []).
+setEquals(Value, [LH | LT]) :-
+	Value #= LH,
+	setEquals(Value, LT).
+
+startSetEquals([LH |LT]) :-
+	setEquals(LH, LT).
+
+
+
+
+addShelfSpace([], []).
+addShelfSpace([LH |LT], [ListH | ListT]) :-
+	getSizeOfBook(SizeOfBook, LH),
+	setBookSpaces(Spaces, SizeOfBook),
+	ListH = [LH, Spaces],
+	addShelfSpace(LT, ListT).
+
+getShelvesSpaces([],[]).
+getShelvesSpaces([[_, ShelfH] | BooksT], [ShelfH |ShelfT]) :-
+	getShelvesSpaces(BooksT, ShelfT).
+
+livros(Books, NrShelves, MaxWidthShelf, MaxHeight):-
+	%length(Books, N),
+	listBooks(Books, MaxHeight),
+
+	
+
+	getShelvesSpaces(Books, ShelveSpaces),
+
+	flatten(ShelveSpaces, ShelveSpacesFlattened),
+
+	domain(ShelveSpacesFlattened, 1, NrShelves),
+	
+	initializeShelves(ShelveSpacesFlattened, NrShelves, MaxWidthShelf, 0),
+
+	labeling([], ShelveSpacesFlattened).
+
+
+/*flatten(Res, List), write(Res),
+domain(List, 1, N),
+
+
+all_distinct(List),
+
+labeling([],List),
+write(Res)*/
+
+
+flatten([],[]).
+flatten([LH|LT], Flattened) :-
+	is_list(LH),
+	flatten(LH, FlattenedTemp),
+	append(FlattenedTemp, LT2, Flattened),
+	flatten(LT, LT2).
+	
+flatten([LH | LT], [LH | FlattenedT]) :-
+	\+is_list(LH),
+	flatten(LT, FlattenedT).
+
+
+initializeShelves(_,Max, _, Max).
+initializeShelves(SSpaces, NrShelves, MaxWidthShelf, CurrShelf):-
+	NextShelf is CurrShelf + 1,
+	count(NextShelf, SSpaces, #=<, MaxWidthShelf), 
+	initializeShelves(SSpaces, NrShelves, MaxWidthShelf, NextShelf). 
+
+	
 
